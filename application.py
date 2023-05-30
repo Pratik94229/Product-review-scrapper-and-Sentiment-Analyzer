4d902fc5f4418e2544fd068f794eea565de55da7
Pratik94229 <pratik.941@gmail.com>
2023-05-31 00:15:00 +0530

    completed creating flipkart review scrapper

--- application.py
+++ application.py
@@ -71,7 +71,7 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://pratik.941:logan@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
             db = client['review_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
